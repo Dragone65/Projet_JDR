4bea1e024a5eab1f61f35a91455b474059ae2788
Dragone65 <florian.bruguier65@outlook.fr>
2025-03-19 11:46:50 +0100

    Ajout de ce qu'a fais le prof

--- Assets/Script/Koboct/Services/ServiceCreationPersonnage.cs
+++ Assets/Script/Koboct/Services/ServiceCreationPersonnage.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Koboct.Data;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Koboct.Services
 {
@@ -10,12 +13,26 @@ namespace Koboct.Services
         [SerializeField] private Personnage _monPersonnage;
 
         [SerializeField] private ServiceLancerDeDe _monServiceDeLanceDeDe;
+
         [SerializeField] private int[] _monResultatJetCharacteristique;
+
+        [FormerlySerializedAs("_listeRacesDisponible")]
+        public List<Race> ListeRacesDisponible = new();
+
+        [FormerlySerializedAs("_listeProfilsDisponible")]
+        public List<Profil> ListeProfilsDisponible = new();
 
         private void OnEnable()
         {
             Reset();
         }
+
+        public Characteristique GetCharacteristique(TypeCharacteristique type)
+        {
+            return _monPersonnage.GetCharacteristique(type);
+        }
+
+        public int[] MonResultatJetCharacteristique => _monResultatJetCharacteristique;
 
         public void SetNomJoueur(string nomJoueur)
         {
@@ -27,19 +44,32 @@ namespace Koboct.Services
             _monResultatJetCharacteristique = null;
         }
 
-        [ContextMenu("Lancer Dé Charactèristique")]
+        [ContextMenu("Lancer Dé Caractèristique")]
         public void LancerDeCharacteristique()
         {
             _monPersonnage.Reset();
-            _monServiceDeLanceDeDe.LancerDesCharacteristiques(RetourResultatLancerCharacterisque);
+            _monServiceDeLanceDeDe.LancerDesCharacteristiques(RetourResultatLancerCaracterisque);
         }
 
-        public void RetourResultatLancerCharacterisque(int[] resultat)
+        public void LancerDeCharacteristiqueAvecValidation()
         {
-            _monResultatJetCharacteristique = resultat;
+            _monPersonnage.Reset();
+            _monServiceDeLanceDeDe.LancerDesCharacteristiques(RetourResultatLancerCaracterisqueValid);
         }
 
-        [ContextMenu("Valider Dé Charactèristique")]
+        public void RetourResultatLancerCaracterisque(int[] resultat)
+        {
+            _monResultatJetCharacteristique = resultat.OrderByDescending(v => v).ToArray();
+        }
+
+        public void RetourResultatLancerCaracterisqueValid(int[] resultat)
+        {
+            _monResultatJetCharacteristique = resultat.OrderByDescending(v => v).ToArray();
+            if (!ValiderResultatDes())
+                LancerDeCharacteristiqueAvecValidation();
+        }
+
+        [ContextMenu("Valider Dé Caractèristique")]
         public void ValiderDeCharacteristique()
         {
             Debug.Log(ValiderResultatDes());
@@ -57,15 +87,35 @@ namespace Koboct.Services
             _monPersonnage.SetCharacterisicValue(TypeCharacteristique.Charisme, _monResultatJetCharacteristique[5]);
         }
 
-        private bool ValiderResultatDes()
+        public bool ValiderResultatDes()
         {
             int sum = 0;
             for (int i = 0; i < 6; i++)
             {
                 sum += Characteristique.CalculModificateur(_monResultatJetCharacteristique[i]);
             }
+#if UNITY_EDITOR
+            Debug.Log(sum);
+#endif
+            return sum >= 3;
+        }
 
-            return sum > 3;
+        public void SetCharacteristique(TypeCharacteristique myCarac, int selectedValue)
+        {
+            _monPersonnage.SetCharacterisicValue(myCarac, selectedValue);
+        }
+
+        public void ChangeRace(Race race)
+        {
+            var actualRace = _monPersonnage.Race;
+            if (actualRace != null)
+                actualRace.RemoveCharacteristiqueModificateur(_monPersonnage);
+
+
+            if (race == null) return;
+
+            _monPersonnage.Race = race;
+            _monPersonnage.Race.ApplyCharacteristiqueModificateur(_monPersonnage);
         }
     }
 }
